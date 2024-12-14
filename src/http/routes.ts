a48eee558bc5f3f6a1fbf6bd979ffdf67100e803
Jean Nunes <jean.nunes@uzzipay.com>
2024-12-14 17:37:56 -0400

    Add auth tests and controller

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from "fastify";
+import { authenticateCrontroller } from "./controllers/authenticateCrontroller";
 import { registerController } from "./controllers/registerController";
 
 export async function appRoutes(app: FastifyInstance) {
   app.post("/users", registerController);
+  app.post("/sessions", authenticateCrontroller);
 }
