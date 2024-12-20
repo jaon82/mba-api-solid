22b6d7bf5f1c957ec89a95de088183d1068a5208
Jean Nunes <jean.nunes@uzzipay.com>
2024-12-19 21:44:21 -0400

    Validate check-in time

--- src/services/validateCheckInService.spec.ts
+++ src/services/validateCheckInService.spec.ts
@@ -1,5 +1,5 @@
 import { InMemoryCheckInsRepository } from "@/repositories/inMemory/CheckInsRepository";
-import { afterEach, beforeEach, describe, expect, it } from "vitest";
+import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
 import { ResourceNotFoundError } from "./errors/ResourceNotFoundError";
 import { ValidateCheckInService } from "./validateCheckInService";
 
@@ -10,11 +10,11 @@ describe("Validate Check-in Service", () => {
   beforeEach(async () => {
     checkInsRepository = new InMemoryCheckInsRepository();
     sut = new ValidateCheckInService(checkInsRepository);
-    // vi.useFakeTimers()
+    vi.useFakeTimers();
   });
 
   afterEach(() => {
-    // vi.useRealTimers()
+    vi.useRealTimers();
   });
 
   it("should be able to validate the check-in", async () => {
@@ -35,5 +35,20 @@ describe("Validate Check-in Service", () => {
         checkInId: "inexistent-check-in-id",
       })
     ).rejects.toBeInstanceOf(ResourceNotFoundError);
+  });
+
+  it("should not be able to validate the check-in after 20 minutes of its creation", async () => {
+    vi.setSystemTime(new Date(2023, 0, 1, 13, 40));
+    const createdCheckIn = await checkInsRepository.create({
+      gym_id: "gym-01",
+      user_id: "user-01",
+    });
+    const twentyOneMinutesInMs = 1000 * 60 * 21;
+    vi.advanceTimersByTime(twentyOneMinutesInMs);
+    await expect(() =>
+      sut.execute({
+        checkInId: createdCheckIn.id,
+      })
+    ).rejects.toBeInstanceOf(Error);
   });
 });
--- src/services/validateCheckInService.ts
+++ src/services/validateCheckInService.ts
@@ -1,5 +1,7 @@
 import { ICheckInsRepository } from "@/repositories/ICheckInsRepository";
 import { CheckIn } from "@prisma/client";
+import dayjs from "dayjs";
+import { LateCheckInValidationError } from "./errors/LateCheckInValidationError";
 import { ResourceNotFoundError } from "./errors/ResourceNotFoundError";
 
 interface ValidateCheckInServiceRequest {
@@ -17,6 +19,15 @@ export class ValidateCheckInService {
     if (!checkIn) {
       throw new ResourceNotFoundError();
     }
+
+    const distanceInMinutesFromCheckInCreation = dayjs(new Date()).diff(
+      checkIn.created_at,
+      "minutes"
+    );
+    if (distanceInMinutesFromCheckInCreation > 20) {
+      throw new LateCheckInValidationError();
+    }
+
     checkIn.validated_at = new Date();
     await this.checkInsRepository.save(checkIn);
     return {
