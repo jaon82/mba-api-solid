aec5ec88c93b8cbc613c161aba4ed536bcecc4ac
Jean Nunes <jean.nunes@uzzipay.com>
2024-12-24 15:22:28 -0400

    Add controller to fetch gyms

---
+++ src/http/controllers/gyms/nearbyController.ts
@@ -0,0 +1,26 @@
+import { makeFetchNearbyGymsService } from "@/services/factories/makeFetchNearbyGymsService";
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+
+export async function nearbyController(
+  request: FastifyRequest,
+  reply: FastifyReply
+) {
+  const nearbyGymsQuerySchema = z.object({
+    latitude: z.number().refine((value) => {
+      return Math.abs(value) <= 90;
+    }),
+    longitude: z.number().refine((value) => {
+      return Math.abs(value) <= 180;
+    }),
+  });
+  const { latitude, longitude } = nearbyGymsQuerySchema.parse(request.body);
+  const fetchNearbyGymsService = makeFetchNearbyGymsService();
+  const { gyms } = await fetchNearbyGymsService.execute({
+    userLatitude: latitude,
+    userLongitude: longitude,
+  });
+  return reply.status(200).send({
+    gyms,
+  });
+}
--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,6 +1,13 @@
 import { verifyJWT } from "@/http/middlewares/verify-jwt";
 import { FastifyInstance } from "fastify";
+import { createController } from "./createController";
+import { nearbyController } from "./nearbyController";
+import { searchController } from "./searchController";
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook("onRequest", verifyJWT);
+
+  app.get("/gyms/search", searchController);
+  app.get("/gyms/nearby", nearbyController);
+  app.post("/gyms", createController);
 }
