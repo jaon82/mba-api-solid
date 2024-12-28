73086178bb756bdec7649b8227146ddee1659120
Jean Nunes <jean.nunes@uzzipay.com>
2024-12-28 01:11:07 -0400

    Set refresh token

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -2,10 +2,12 @@ import { FastifyInstance } from "fastify";
 import { verifyJWT } from "../../middlewares/verify-jwt";
 import { authenticateCrontroller } from "./authenticateCrontroller";
 import { profileCrontroller } from "./profileCrontroller";
+import { refreshController } from "./refreshController";
 import { registerController } from "./registerController";
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post("/users", registerController);
   app.post("/sessions", authenticateCrontroller);
+  app.patch("/token/refresh", refreshController);
   app.get("/me", { onRequest: [verifyJWT] }, profileCrontroller);
 }
